ef984860652a885e94b2970fa6f3d7f8380922c2
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-08-15 11:28:15 +0530

    Setup static files

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
  const express = require('express');
-
  const app = express();
-
  const port = 8000;
+ const expressLayouts = require('express-ejs-layouts');
+
+ app.use(express.static('./assets'));
+
+ app.use(expressLayouts);
+
+ //Extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //Use express router
 app.use('/', require('./routes/index'));
