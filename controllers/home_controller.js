adaf3019e92b27e130fa66e382d0d57154790b92
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-08-21 12:01:04 +0530

    Post Implemented

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,24 @@
-
+const Post = require('../models/post');
+const { populate } = require('../models/post');
 //module.export.actionName = function(req,res){};
 
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title : "Home"
-    });
-    
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title : "Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+    //Populate user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home', {
+                    title : "Codeial | Home",
+                    posts : posts
+         });
+    })
+
    // return res.end('<h1>Express is up for Codeial!</h1>');
 }
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
+const { route } = require('./posts');
 
 //console.log("Router loaded");
 
 router.get('/', homeController.home);
 
 router.use('/users', require('./users'));
+
+router.use('/posts', require('./posts'));
 
 //For any further routes access from here
 //router.use('/routerName', require('./routerfile'));
