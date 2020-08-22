174723208660603d942d81f5663c4ae840751e3d
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-08-22 11:41:35 +0530

    Showing Comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,18 @@ module.exports.home = function(req,res){
     //     });
     // });
     //Populate user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    //Need to populate multiple models, first 
+    //comments and then user of the comment
+    .populate({
+        path : 'comments',
+        //Further populate
+        populate : {
+            path : 'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home', {
                     title : "Codeial | Home",
                     posts : posts
