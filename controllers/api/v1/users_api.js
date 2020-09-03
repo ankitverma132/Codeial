8aafa7efaa826a08450e9e9688906f42742283e8
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-09-03 15:22:05 +0530

    JWT

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,30 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+//Get the signin data
+module.exports.createSession = async function(req,res){
+    try{
+        let user = User.findOne({
+            email : req.body.email
+        });
+        if(!user || user.password != req.body.password){
+            return res.json(422,{
+                message : "Invalid username or password"
+            });
+        }
+        //if user found
+        return res.json(200, {
+            message : "Sign in succesful, here is your token, please keep it safe",
+            data : {
+                //Passing user
+                token : jwt.sign(user.toJSON(), 'codeial', {expiresIn : '10000'})
+            }
+        })
+
+    }catch(err){
+        console.log('****Error');
+        return res.json(500, {
+            message : "Internal server error"
+        });
+    }
+}
