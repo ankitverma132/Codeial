99fb9068f6653d7406e1c0e091f721551aaf9d3e
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-09-07 22:01:55 +0530

    nodemailer config

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,13 +1,39 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
 
-//Difine transport
-let transport = nodemailer.createTransport({
+//Define transport
+//This is the part which sends email
+let transporter = nodemailer.createTransport({
     service : 'gmail',
     host : 'smtp.gmail.com',
     port : 587,
     //Not using 2-factor authentication
     secure : false,
     auth : {
-
+        user : 'ankitv.ashokaec19@gmail.com',
+        pass : 'Ankitverma@8960'
     }
-})
+});
+
+//Now we need to define that  we will use ejs
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile{
+        path.join(__dirname, '../views/mailers', relativePath);
+        data,
+        function(err,template){
+            if(err){
+                console.log('Error in rendering template');
+                return; 
+            }
+            mailHTML = template;
+        }
+    }
+    return mailHTML;
+}
+
+module.exports = {
+    transporter : transporter,
+    renderTemplate : renderTemplate
+}
